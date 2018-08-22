eb7772b9436add45adb327ee16e4946d38e30065
Jesse Wallace <jjaywallace@gmail.com>
2018-08-22 15:02:16 -0700

    Scatter Plot Component Init - Read me Update

---
+++ src/ScatterPlot.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ScatterPlot.css';
+
+class ScatterPlot extends Component {
+    render() {
+        return (
+            <div className="ScatterPlot">
+                TEST
+            </div>
+        );
+    }
+}
+
+export default ScatterPlot;
